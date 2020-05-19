ee98660f473f75869983b94ba2488530aae42625
yakushgleb <60699192+yakushgleb@users.noreply.github.com>
2020-05-19 19:04:04 +0300

    feat: add countries filter to reducer

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -38,6 +38,42 @@ const appReducer = (state = initialState, action) => {
                 },
             }
 
+        case COUNTRY_FILTER_ALPHABET:
+            return {
+                ...state,
+                countriesList: {
+                    ...state.countriesList,
+                    filtered: [...state.countriesList.original]
+                },
+            }
+
+        case COUNTRY_FILTER_CONFIRMED:
+            return {
+                ...state,
+                countriesList: {
+                    ...state.countriesList,
+                    filtered: [...state.countriesList.filtered.sort((a, b) => b.TotalConfirmed - a.TotalConfirmed)]
+                },
+            }
+
+        case COUNTRY_FILTER_RECOVERED:
+            return {
+                ...state,
+                countriesList: {
+                    ...state.countriesList,
+                    filtered: [...state.countriesList.filtered.sort((a, b) => b.TotalRecovered - a.TotalRecovered)]
+                },
+            }
+
+        case COUNTRY_FILTER_DEATH:
+            return {
+                ...state,
+                countriesList: {
+                    ...state.countriesList,
+                    filtered: [...state.countriesList.filtered.sort((a, b) => b.TotalDeaths - a.TotalDeaths)]
+                },
+            }
+
         default:
             return state
     }
