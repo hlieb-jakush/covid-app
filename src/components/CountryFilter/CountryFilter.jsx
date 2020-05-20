d28b7a811b9330f61aa97b9bf1b56b6c1bbf5dc6
yakushgleb <60699192+yakushgleb@users.noreply.github.com>
2020-05-20 13:04:00 +0300

    fix: rename fields in countries filter

--- src/components/CountryFilter/CountryFilter.jsx
+++ src/components/CountryFilter/CountryFilter.jsx
@@ -4,15 +4,15 @@ import './CountryFilter.scss'
 const CountryFilter = ({ setCountryFilterAlphabet, setCountryFilterConfirmed, setCountryFilterRecovered, setCountryFilterDeath }) => {
     return (
         <section className='filter'>
-            <span>Sorted by:</span>
             <span>
-                <button onClick={setCountryFilterAlphabet}>Alphabet</button>
+                <span>Sorted by:</span>
+                <button onClick={setCountryFilterAlphabet}>Default</button>
                 <button onClick={setCountryFilterConfirmed}>Confirmed</button>
                 <button onClick={setCountryFilterRecovered}>Recovered</button>
                 <button onClick={setCountryFilterDeath}>Deaths</button>
             </span>
             <span>
-                <input placeholder='Title...' />
+                <input placeholder='Country title...' />
                 <button>Search</button>
             </span>
         </section>
