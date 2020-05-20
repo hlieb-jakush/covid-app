a083e41f1c8fdcc9c808ba3e5b0a60b609437969
yakushgleb <60699192+yakushgleb@users.noreply.github.com>
2020-05-20 17:42:43 +0300

    fix: set correct link in sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -15,13 +15,13 @@ const Sidebar = () => {
     return (
         <aside>
             <NavLink exact to='/' activeClassName="selected"><Button icon={world} country='World' /></NavLink>
-            <NavLink to='/united%20kingdom' activeClassName="selected"><Button icon={uk} country='United Kingdom' /></NavLink>
+            <NavLink to='/united-kingdom' activeClassName="selected"><Button icon={uk} country='United Kingdom' /></NavLink>
             <NavLink to='/france' activeClassName="selected"><Button icon={france} country='France' /></NavLink>
             <NavLink to='/germany' activeClassName="selected"><Button icon={germany} country='Germany' /></NavLink>
             <NavLink to='/italy' activeClassName="selected"><Button icon={italy} country='Italy' /></NavLink>
             <NavLink to='/belarus' activeClassName="selected"><Button icon={belarus} country='Belarus' /></NavLink>
             <NavLink to='/ukraine' activeClassName="selected"><Button icon={ukraine} country='Ukraine' /></NavLink>
-            <NavLink to='/russian%20federation' activeClassName="selected"><Button icon={russia} country='Russia' /></NavLink>
+            <NavLink to='/russia' activeClassName="selected"><Button icon={russia} country='Russia' /></NavLink>
         </aside>
     )
 }
