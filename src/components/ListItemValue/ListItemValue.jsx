d029546b949bfe5c4bf44047c09b453836170047
yakushgleb <60699192+yakushgleb@users.noreply.github.com>
2020-05-29 14:54:10 +0300

    Revert "Revert "feat: add ListItemVlue to header""
    This reverts commit a400427755bf662b0f748d9d890ae75eb7670959.

--- src/components/ListItemValue/ListItemValue.jsx
+++ src/components/ListItemValue/ListItemValue.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './ListItemValue.scss'
 
-const ListItemValue = ({ text, totalData, numberConverter, dateConverter }) => {
+const ListItemValue = ({ img, alt, text, totalData, newData, numberConverter, dateConverter, isColumn }) => {
     return (
-        <span className='list-item__value'>
+        <span className={`list-item__value ${isColumn ? 'list-item__value_column' : ''}`}>
+            {img && <span className='list-item__value_text'><img src={img} alt={alt} /> </span>}
             {text && <span className='list-item__value_text'>{text} </span>}
             {dateConverter && <span className='list-item__value_data'>{dateConverter(totalData)}</span>}
-            {numberConverter && <span className='list-item__value_data'>{numberConverter(totalData)}</span>}
+            {numberConverter && !newData && <span className='list-item__value_data'>{numberConverter(totalData)}</span>}
+            {numberConverter && !!newData && <span className='list-item__value_data'>{numberConverter(totalData)} (+{numberConverter(newData)})</span>}
         </span>
     )
 }
